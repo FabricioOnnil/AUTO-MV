045b1cd75b827f6af1a586ff21f4c730f5353946
Fabricio Rocha Teixeira <frt.fabricio@gmail.com>
2024-05-31 20:33:35 -0300

    .

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,16 +1,16 @@
 function goToSchedule() {
     // Redirecionar para a página agenda.html
-    window.location.href = "/agenda.html";
+    window.location.href = "agenda.html";
      }
 function goToDeliver() {
    // Redirecionar para a página entrega.html
-   window.location.href = "/entrega.html";
+   window.location.href = "entrega.html";
      }
 function goToLocation() {
    // Redirecionar para a página mapa.html
-   window.location.href = "/mapa.html";
+   window.location.href = "mapa.html";
      }
 function goToDriving() {
    // Redirecionar para a pagina trajeto.html
-   window.location.href = "/execute.html";
+   window.location.href = "execute.html";
      }
