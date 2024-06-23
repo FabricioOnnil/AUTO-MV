6c5651fee24dbf5ea6986151c13cdf0c7faa93d1
Fabricio Rocha Teixeira <frt.fabricio@gmail.com>
2024-06-23 09:17:52 -0300

    .

--- BancoMV.session.sql
+++ BancoMV.session.sql
@@ -1,15 +1,4 @@
 use vamo_auto_mv;
 
 show tables;
- select * from usuario;
-
- UPDATE  usuario set senha_usuario = 'senha1' where id_usuario = 2;
-UPDATE usuario 
-SET primeiro_nome = 'Carlos', 
-    segundo_nome = 'Amelia', 
-    senha_usuario = 'senha1' 
-WHERE id_usuario = 2;
-
-INSERT INTO abastecimento (descricao_abast, valor_abast, data_abast)
-VALUES ('Teste1', 10.00, '2024-05-26');
-
+ select * from user;
