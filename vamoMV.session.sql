b7dec955284e4f1649303fd96208904cf3e8f0fc
Fabricio Rocha Teixeira <frt.fabricio@gmail.com>
2024-07-17 15:42:59 -0300

    .

--- vamoMV.session.sql
+++ vamoMV.session.sql
@@ -9,3 +9,5 @@ INSERT INTO usuario (s_usuario_name, s_usuario_secondName, s_usuario_password, i
 SELECT * From usuario;
 
 INSERT INTO usuario (s_usuario_name, s_usuario_secondName, s_usuario_password, i_usuario_licenseDriving, s_usuario_sectorShipping, d_usuario_dateExpiration) VALUES ('Pedro','Claudio','222222','865741','Detran-SP','2035-06-17');
+
+SELECT * FROM usuario;
