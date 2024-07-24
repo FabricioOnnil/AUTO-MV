fb0005f77a6656dff0c50ff9a8d90648db52cfa3
Fabricio Rocha Teixeira <frt.fabricio@gmail.com>
2024-07-24 16:46:12 -0300

    .

--- script/server.js
+++ script/server.js
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
   } catch (error) {
     console.error("Falha ao se conectar ao banco de dados ou sincronizar modelos:", error);
   }
-})();
+});
 
 // Configuração do diretório de views
 app.set('views', join(__dirname, '..', 'views'));
