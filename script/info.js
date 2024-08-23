4ec5f3db18548b1e8760f986570d5041162b497d
Fabricio Rocha Teixeira <frt.fabricio@gmail.com>
2024-08-23 14:30:38 -0300

    .

--- script/info.js
+++ script/info.js
@@ -1,11 +1,33 @@
-function openPopup(url) {
-    window.open(url, '_blank', 'width=800,height=600');
-}
-
 function abrirPopup(popupId) {
     document.getElementById(popupId).style.display = 'flex';
+}
+
+function openPopup(url) {
+    window.open(url, 'popupWindow', 'width=800,height=600, scrollbars=yes');
 }
 
 function closePopup(popupId) {
     document.getElementById(popupId).style.display = 'none';
 }
+
+document.getElementById('longTextForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const longText = document.getElementById('longText').value;
+    const message = document.getElementById('message');
+
+    if (longText.length > 0) {
+        
+        if (longText.length >= 10) {
+            message.textContent = 'Texto enviado com sucesso!';
+            message.style.color = 'green';
+            
+        } else {
+            message.textContent = 'O texto deve ter pelo menos 10 caracteres.';
+            message.style.color = 'red';
+        }
+    } else {
+        message.textContent = 'O campo não pode estar vazio.';
+        message.style.color = 'red';
+    }
+});
